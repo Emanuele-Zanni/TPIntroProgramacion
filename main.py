9f2843ac707a2353a9d14e756b4837fc983d2625
Emanuele-Zanni <emazanni2022@gmail.com>
2025-10-08 15:17:30 -0300

    Finalizada feature de -Levantar Mesa-

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ app=True
 mainMenu=True
 mesasMenu=False
 mozosMenu=False
-Mozos=[]
-Stock=[]
+stockMenu=False
+mozos=["mozo1","mozo2","mozo3"]
+
+#* Type Mesa = NumeroMesa??(Se puede hacer implicito con la posicion del elemento en el array),
+#* MozoAsignado,"Pedidos",Disponible?(Boolean),MontoAPagar,
+# mesas = [{1,0,"",True,0},{2,0,"",True,0},{3,0,"",True,0},{4,0,"",True,0},{5,0,"",True,0}] #* Con Objetos + numMesa
+mesasViejas = [{0,"",False,0},{0,"",True,0},{0,"",True,0},{0,"",True,0},{0,"",True,0}] #* Con Objetos sin numMesa
+mesas = [[0,"",False,0],[0,"",True,0],[0,"",True,0],[0,"",True,0],[0,"",True,0]] #* Con Listas sin numMesa
+#? Resolucion para el problema de mesas = tratarlas como listas de listas, o manejar objetos con campos nombrados,
+#? es decir, con {"clave" : valor}
+
+mesasDelivery = [] #* mesas infinitas, mismo type de mesa pero SIN estado "disponible?", y numMesa modificado (un id)
+pedidos = [] #* xd? (creo q era solo para cumplir con consigna semana 2)
 
 while app:
     while mainMenu:
         print("""
-    1)Mesas
-    2)Mozos
-    3)Stock
-    4)Cajas
-    5)Finalizar Dia
+    1)Mesas (Work in progress)
+    2)Mozos (FALTA)
+    3)Stock (FALTA)
+    4)Cajas (FALTA)
+    5)Finalizar Dia (FALTA)
     """)
 
         choice=input("Ingrese una opcion: ")
@@ -37,24 +48,22 @@ while app:
         else:
             print("Opcion invalida")
 
-    #* Type Mesa = NumeroMesa??(Se puede hacer implicito con la posicion del elemento en el array),
-    #* MozoAsignado,"Pedidos",Disponible?(Boolean),MontoAPagar,
-    # mesas = [{1,0,"",True,0},{2,0,"",True,0},{3,0,"",True,0},{4,0,"",True,0},{5,0,"",True,0}] #* Con Objetos + numMesa
-    mesas = [{0,"",True,0},{0,"",True,0},{0,"",True,0},{0,"",True,0},{0,"",True,0}] #* Con Objetos sin numMesa
-
-    #? Resolucion para el problema de mesas = tratarlas como listas de listas, o manejar objetos con campos nombrados,
-    #? es decir, con {"clave" : valor}
-
-
-    mesasDelivery = [] #* mesas infinitas, mismo type de mesa pero SIN estado "disponible?", y numMesa modificado (un id)
-    pedidos = []
+    #* Submenu Mesas
+    #* 1) Levantar Mesa 
+    #* 2) Editar/Acceder Mesa => submenu => Mover Mesa, Cambiar Mozo, Editar Pedidos, Cobrar Mesa, Anular Mesa, Convertir a Delivery
+    #* 3) Cobrar Mesa
+    #* 4) Anular Mesa
 
     while mesasMenu:
+
         print("""
-    1)Ver mesas (Crear submenu?)
+    1)Ver Mesas/Salon (Crear submenu?)
+    11)Levantar Mesa
+    22)Ver Delivery
+    33)Ver TODO
+    
     2)Agregar pedido (Va a ser reemplazado por "Levantar Mesa")
     3)Ver pedidos (Esto vuela, implicito en Ver Mesas)
-    11)Levantar Mesa
     X)Volver al menu principal
     """)
 
@@ -86,7 +95,7 @@ while app:
                 
         elif choice=="3":
             print(pedidos)
-        #! LEVANTAR MESA
+        #! FUNCION LEVANTAR MESA
         elif choice=="11":
             pedidosMesa = []
             mesaValida=False
@@ -94,34 +103,50 @@ while app:
             pedidoValido=False
             #* Type Mesa = NumeroMesa,MozoAsignado,"Pedidos",Disponible?(Boolean),MontoAPagar
 
+            #* Validar Mesa
             while mesaValida==False:
                 numMesa = int(input("Ingrese el numero de mesa: "))
+                tableFound=False
 
                 for i in range(len(mesas)):
-                    if mesas[numMesa-1] != mesas[i]:
-                        print("Mesa no encontrada")
-                    elif mesas[numMesa-1] == False: #* Disponible? = False
-                        print("Mesa ocupada")
-                    else:
-                        mesaValida=True
+                    if numMesa-1 != i and tableFound==False:
+                        if i+1 == len(mesas):
+                            print("Mesa no encontrada")
+                    elif mesas[numMesa-1][2] == False: #* Disponible? = False
+                        tableFound=True
+                        if i+1 == len(mesas):
+                            print("Mesa ocupada")
                         # break
+                    elif numMesa-1 == i and tableFound==False:
+                        print("Mesa encontrada exitosamente")
+                        mesaValida=True
+                        tableFound=True
+                        i = len(mesas)
+                        # break
+
+            while mozoValido==False:
+                numMozo = int(input("Ingrese el numero de mozo: "))
 
                 for i in range(len(mozos)):
-                    if mesas[numMesa-1] != mesas[i]:
-                        print("Mesa no encontrada")
-                    elif mesas[numMesa-1] == False: #* Disponible? = False
-                        print("Mesa ocupada")
+                    if numMozo-1 != i:
+                        if i+1 == len(mozos):
+                            print("Mozo no encontrado")
                     else:
-                        mesaValida=True
+                        mozoValido=True
+                        print("Mozo encontrado exitosamente")
+                        print(f"Mozo numero {numMozo}: {mozos[numMozo-1]}")
                         # break
 
-            
-    
-            numMozo = int(input("Ingrese el numero de mozo: "))
-            pedidosMesa = input("Ingrese los pedidos: ")
-
+            item = "placeholder"
+            while item != "":
+                item = input("Ingrese un item o ingrese enter vacio para finalizar la carga: ")
+                if item!= "":
+                    pedidosMesa.append(item)
+                else:
+                    print("Carga de pedidos finalizada")
+                
             #* Esto reemplazar por funcion LevantarMesa()
-            mesas[numMesa-1] = {numMozo,pedidosMesa,False,0}
+            mesas[numMesa-1] = [mozos[numMozo-1],pedidosMesa,False,0]
         #! EDITAR MESA
         elif choice=="12": 
         # print: Mover mesa, Cambiar Mozo, Editar Pedidos, Cobrar Mesa, Anular Mesa, Convertir a Delivery/Salon
